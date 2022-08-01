8c09d290f698ed31978a59fc91851b28cd96b1e4
Kararetia <dovhun.natia2000@gmail.com>
2022-08-01 16:46:02 +0300

    Initial Commit

---
+++ Weather2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Weather2
+//
+//  Created by Dima  on 01.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
